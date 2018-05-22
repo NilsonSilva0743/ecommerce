4c897fe04c6b193fb3f7022748c3b6737ba12558
Nilson Silva <nalcantara0743@gmail.com>
2018-05-22 10:20:33 -0300

    Carrinho de Compras

--- site.php
+++ site.php
@@ -3,6 +3,8 @@
 use \Hcode\Page;
 use \Hcode\Model\Product;
 use \Hcode\Model\Category;
+use \Hcode\Model\Cart;
+
 
 
 $app->config('debug', true);
@@ -65,6 +67,16 @@ $app->get("/products/:desurl", function($desurl){
 		'product'=>$product->getValues(),
 		'categories'=> $product->getCategories()
 	]);
+});
+
+$app->get("/cart", function(){
+
+	$cart = Cart::getFromSession();
+
+	$page = new Page();
+
+	$page->setTpl("cart");
+
 });
 
 
